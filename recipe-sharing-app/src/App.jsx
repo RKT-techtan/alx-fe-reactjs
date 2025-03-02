cae1b49666cb4bb7c3551bfaaaf245696d33670f
RKT-techtan <fandfilian098@gmail.com>
2025-03-02 16:07:31 +0300

    This project will cover essential concepts and techniques for state management, culminating in the creation of a Recipe Sharing Application. By integrating Zustand, you will enhance your ability to manage complex state interactions in a scalable and maintainable way.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,3 +1,5 @@
+import React from './react'
+import { BrowserRouter as Router, Routes,Route } from './react-router-dom'
 import RecipeDetails from './RecipeDetails'
 import EditRecipeForm from './EditRecipeForm'
 import DeleteRecipeButton from './DeleteRecipeButton'
