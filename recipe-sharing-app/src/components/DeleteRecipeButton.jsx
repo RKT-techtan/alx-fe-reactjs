3ad2f2f017eb0d2119dd07354d8de516d14657cf
RKT-techtan <fandfilian098@gmail.com>
2025-03-02 14:58:03 +0300

    This project will cover essential concepts and techniques for state management, culminating in the creation of a Recipe Sharing Application. By integrating Zustand, you will enhance your ability to manage complex state interactions in a scalable and maintainable way.

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -5,7 +5,10 @@
     const recipe = useRecipeStore(state =>
       state.recipes.find(recipe => recipe.id === recipeId)
     );
-	  const navigate = useNavigate();
+
+	  deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter((recipe) => recipe.id !== id) })),
+	  
+		  const navigate = useNavigate();
 
 
     return (
@@ -14,5 +17,7 @@
         <p>{recipe.description}</p>
         {/* Render EditRecipeForm and DeleteRecipeButton here */}
       </div>
+	    <div><button onClick={this.call}>Button
+	    </button></div>
     );
   };
