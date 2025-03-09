58123b3f01048ca913e6cabeb6c08ef931919978
RKT-techtan <fandfilian098@gmail.com>
2025-03-09 15:24:23 +0300

    In the “Advanced React and Authentication in React JS” project, you will delve into more sophisticated aspects of React, focusing on advanced form handling, data management, routing, and testing. This project will enhance your skills in creating robust and scalable React applications while integrating essential features such as user authentication and API interactions.

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,1 +1,25 @@
-iii
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const const FormikForm = () => (
+    <Formik
+        initialValues={{ name: '', email: '' }}
+        validationSchema={validationSchema}
+        onSubmit={(values) => {
+            console.log(values);
+        }}
+    >
+        {() => (
+            <Form>
+                <Field type="text" name="name" />
+                <ErrorMessage name="name" component="div" />
+                <Field type="email" name="email" />
+                <ErrorMessage name="email" component="div" />
+                <button type="submit">Submit</button>
+            </Form>
+        )}
+    </Formik>
+);
+
+
+export default FormikForm;
