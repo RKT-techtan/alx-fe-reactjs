4a3fe6b89f6ab3dd1191d9ff8944dd01c242072c
RKT-techtan <fandfilian098@gmail.com>
2025-03-19 09:10:57 +0300

    Added a Homepage using tailwindcss

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,7 +12,7 @@ function HomePage() {
 			type="text rounded-md shadow-md"
 			/>
 		);
-		<div className="Homepage lg:W-2/3 sm:W-1/4 hover:bg-green-300">
+		<div className="Homepage lg:W-2/3 sm:W-1/4 hover:bg-green-300 grid grid-cols-1 grid-cols-2 grid-cols-3 ">
 		<img src="Image lg:w-2/3" alt="user hover:bg-green-300" />
 		<h1>Home Page</h1>
 		</div>
