cca5de0274d619f2d84a0c21125ec56a561fcc0e
RKT-techtan <fandfilian098@gmail.com>
2025-03-23 23:06:31 +0300

    Added recipe detailed information page

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import data from './data.json';
 import react from './react'
 
@@ -17,6 +17,7 @@ function HomePage() {
 		<div className="Homepage lg:W-2/3 sm:W-1/4 hover:bg-green-300 grid grid-cols-1 grid-cols-2 grid-cols-3 ">
 		<img src="Image lg:w-2/3" alt="user hover:bg-green-300" />
 		<h1>Home Page</h1>
+			<Link to={'${url}}></link>
 		</div>
 	);
 }
