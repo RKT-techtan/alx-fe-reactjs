430bd9f160a1396e13358788766d24eaf3608eeb
RKT-techtan <fandfilian098@gmail.com>
2025-03-23 23:31:54 +0300

    Enabled form to include new recipes

---
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -0,0 +1,12 @@
+import { Route, Link, useRouteMatch, useEffect } from 'react-router-dom';
+import { components, useState } from 'react';
+
+function AddRecipeForm () {
+	const [count, setCount] = useState(0);
+	return (
+	<div>
+		<form>
+		</form>
+	</div>
+	);
+};
