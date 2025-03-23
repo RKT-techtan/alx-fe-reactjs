7ae3a6337513ff91e907f8e4fba4fa99aafc3bed
RKT-techtan <fandfilian098@gmail.com>
2025-03-23 22:42:29 +0300

    Added recipe detailed information page

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,9 +1,13 @@
-import { BrowserRouter, Route, Routes, react-router-dom, } from 'react-router-dom';
+import { BrowserRouter, useParams, Route, Routes, react-router-dom, } from 'react-router-dom';
+import data from './data.json';
+import React, { UseEffect } from 'react';
 import { Component } from 'react';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
 
 function RecipeDetail() {
+	const json = await data.json();
+	}
 	return (
 	);
 };
